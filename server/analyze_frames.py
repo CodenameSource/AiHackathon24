50f0ad9f60c60e29d2c68912afa1b9be229fa364
Flychuban <kaloanas07@gmail.com>
2024-10-06 02:25:28 +0300

    add code for cleaned func

---
+++ server/analyze_frames.py
@@ -0,0 +1,61 @@
+import cv2
+import os
+import base64
+from openai import OpenAI
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+FRAME_INTERVAL = 150
+
+def extract_frames(all_frames, frame_interval):
+    frames = []
+    frame_count = 0
+
+    for frame in all_frames:
+        if frame_count % frame_interval == 0:
+            frames.append(frame)
+        frame_count += 1
+        
+    return frames
+
+
+def analyze_frames_with_gpt_vision(frames):
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "You are a reinforcemnt learning expert. This is a gameplay from a 2d game. Explain me what is the purpose of the game - all observations, actions and reward functions. The response should be in a form that is the most meaningful to a LLM for generating a code that will make the environment.",
+                }
+            ],
+        }
+    ]
+
+    for frame in frames:
+        messages[0]["content"].append(
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/jpeg;base64,{frame}",
+                },
+            }
+        )
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=messages,
+        max_tokens=1500,
+        temperature=0.15,
+    )
+    return response
+
+def get_knowledge_from_images(all_frames):
+    frames = extract_frames(all_frames, FRAME_INTERVAL)
+    response = analyze_frames_with_gpt_vision(frames)
+    final_message = response.choices[0].message.content
+    return final_message
--- server/generate_environment.py
+++ server/generate_environment.py
@@ -1,22 +1,22 @@
 from openai import OpenAI
 import os
-from gpt_vision_test import get_knowledge_from_images
+from server.analyze_frames import get_knowledge_from_images
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-def create_gym_env_from_message():
+def create_gym_env_from_message(all_frames):
     # Set your OpenAI API key here or use an environment variable
     client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 
     # Define the context and prompt
     context = """
-    You are a reinforcemnt learning expert. Your task is to create an accurate and good gym environment for 2d game using the knowledge extracted from the gameplay. Please write ONLY the Python code that is creating the environment. The environment should be defined VERY well
+    You are a reinforcemnt learning expert. Your task is to create an accurate and good gym environment for 2d game using the knowledge extracted from the gameplay. Please write ONLY the Python code that is creating the environment. The environment should be defined in a typical way that is used in OpenAI Gym. 
     """
 
-    prompt = get_knowledge_from_images("/Users/flychuban/Downloads/dino_test_video.mkv", 130, "extracted_images")
+    prompt = get_knowledge_from_images(all_frames)
 
 
     messages = [
@@ -43,7 +43,7 @@ def create_gym_env_from_message():
     # Call the OpenAI API to generate the response
     response = client.chat.completions.create(
         model="gpt-4",
-        max_tokens=1500,
+        max_tokens=3000,
         temperature=0.15,
         messages=messages,
     )
